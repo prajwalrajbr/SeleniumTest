4ec33553cb5a2e6b3392e7f247bd160084c61a1e
Prajwal <rajprajwal3@gmail.com>
2022-05-31 12:14:39 +0530

    npm install --save-dev wdio-timeline-reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,4 @@
+const { TimelineService } = require('wdio-timeline-reporter/timeline-service');
 exports.config = {
     //
     // ====================
@@ -110,7 +111,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['chromedriver',[TimelineService]],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -132,7 +133,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','dot','mochawesome'],
+    reporters: ['spec',['timeline', { outputDir: './report' }]],
 
 
     
